4466bf08e839b3ed5f5bb8aafd09e53f057a28e7
christian otkjær <christian.otkjaer@gmail.com>
2016-02-26 12:47:02 +0100

    Initial Commit

---
+++ Settings/Settings.h
@@ -0,0 +1,19 @@
+//
+//  Settings.h
+//  Settings
+//
+//  Created by Christian Otkjær on 26/02/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Settings.
+FOUNDATION_EXPORT double SettingsVersionNumber;
+
+//! Project version string for Settings.
+FOUNDATION_EXPORT const unsigned char SettingsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Settings/PublicHeader.h>
+
+
